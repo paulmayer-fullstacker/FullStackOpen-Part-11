64feb5e3946a60e7698028bafe4cf5e033c42b51
Paul <pmayer.devopseng@gmail.com>
2026-09-13 01:01:11 +0100

    Deployment simulating health check failure

--- pokedex-app/app.js
+++ pokedex-app/app.js
@@ -1,3 +1,5 @@
+// app.js:
+
 const express = require('express')
 const app = express()
 
@@ -5,6 +7,14 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
+
+// Health Check Endpoint: Render sends a GET request to '/health' after building the app.
+// If the endpoint responds with a success (HTTP 200) status, Render knows the server is healthy and will direct traffic to this new deployment.
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  ' // Test Heath Check by simulating failure. Remove during normal use.
+  res.send('ok')
+})
 
 const start = async () => {
   await app.listen(PORT)
